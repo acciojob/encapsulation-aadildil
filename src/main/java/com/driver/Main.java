3fb55bbd4d89c54b0c3b9ff7b35da675b4abf9ac
shabaka computers <aadilvazhakkad@gmail.com>
2023-04-14 16:50:28 +0530

    Implemented encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,6 +5,13 @@ public class Main {
     public static void main(String[] args) {
         RWOnly rw=new RWOnly();
 
+      //  rw.Name="Aadil";
+       // System.out.print(rw.name);
+        //java: Name has private access in com.driver.RWOnly
+
+        rw.setName("Aadil");
+        System.out.print(rw.getName());
+
 
     }
 
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,5 +2,13 @@ package com.driver;
 
 public class RWOnly {
 
+    public String getName() {
+        return Name;
+    }
 
+    public void setName(String name) {
+        Name = name;
+    }
+
+    private String Name;
 }
